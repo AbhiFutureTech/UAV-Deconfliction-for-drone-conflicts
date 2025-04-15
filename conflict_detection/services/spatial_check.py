8a6ef6fb152c7fc6f8f3793ae548325a8035bd5a
Abhijit Patil <157373320+AbhiFutureTech@users.noreply.github.com>
2025-04-15 23:23:13 +0530

    Delete conflict_detection directory

--- conflict_detection/services/spatial_check.py
+++
@@ -1,9 +0,0 @@
-def detect_spatial_conflict(primary_mission, simulated_flights):
-    conflicts = []
-    for flight in simulated_flights:
-        for waypoint in flight["waypoints"]:
-            for primary_waypoint in primary_mission["waypoints"]:
-                if (abs(primary_waypoint["x"] - waypoint["x"]) < 10 and 
-                    abs(primary_waypoint["y"] - waypoint["y"]) < 10):  # 10m buffer
-                    conflicts.append({"location": waypoint, "conflict_with": flight["id"]})
-    return conflicts
